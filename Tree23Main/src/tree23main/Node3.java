d86657c4a4c704a67ed564454312dc6ed59159a9
Dnelre-Slin <nilsheggem@hotmail.com>
2017-03-08 23:55:01 +0100

    first added

---
+++ Tree23Main/src/tree23main/Node3.java
@@ -0,0 +1,10 @@
+package tree23main;
+
+public class Node3 <T extends Comparable> implements NodeI<T>{
+
+    @Override
+    public NodeI append(T append_value) {
+        return null;
+    }
+    
+}
