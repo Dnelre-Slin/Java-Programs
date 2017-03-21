359a2a2b0fbef02da5b92eb5c7ed1b19aa27190f
Dnelre-Slin <nilsheggem@hotmail.com>
2017-03-22 00:02:30 +0100

    first added

--- Graphs/src/graphs/Graph.java
+++ Graphs/src/graphs/Graph.java
@@ -3,14 +3,14 @@ import hashmap.HashMap;
 import hashmap.LinkedList;
 
 public class Graph <Key extends Comparable,T>{
-    HashMap<Key,LinkedList<Key,T>> map;
+    private HashMap<Key,Vertex<Key,T>> map;
     
     public Graph(int size){
         map = new HashMap<>(size);
     }
     
     public void addVertex(Key append_key, T append_value){
-//        map.append(append_key, append_value);
+//        map.append(append_key, new Vertex(append_key,append_value));
     }
     
 //    public void addEdge()
--- Graphs/src/graphs/Vertex.java
+++ Graphs/src/graphs/Vertex.java
@@ -10,7 +10,19 @@ public class Vertex <Key extends Comparable,T>{
         edges = new LinkedList<>();
     }
     
-    public void addEdge(){
-//        edges.append(append_key, core);
+    public void addEdge(Key add_key, Vertex<Key,T> add_vertex){
+        edges.append(add_key, add_vertex);
+    }
+    public void removeEdge(Key remove_key){
+        edges.delete(remove_key);
+    }
+    
+    public Vertex<Key,T> findEdge(Key find_key){
+        LinkedList<Key,Vertex<Key,T>> tmp = edges.search(find_key);
+        return (tmp!=null)?tmp.getValue():null;
+    }
+    
+    public T getCore(){
+        return core;
     }
 }
